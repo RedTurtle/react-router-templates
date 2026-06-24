3cc0386e598a1e1f906a7d5bbc0f89edae13487d
Piero Nicolli <piero.nicolli@redturtle.it>
2026-06-24 17:45:55 +0200

    feat: added react-router-devtools and vite-plugin-devtools-json

--- default/vite.config.ts
+++ default/vite.config.ts
@@ -1,12 +1,19 @@
 import { reactRouter } from '@react-router/dev/vite';
 import tailwindcss from '@tailwindcss/vite';
+import { reactRouterDevTools } from 'react-router-devtools';
 import { defineConfig } from 'vite';
+import devtoolsJson from 'vite-plugin-devtools-json';
 
 export default defineConfig({
   resolve: {
     tsconfigPaths: true,
   },
-  plugins: [tailwindcss(), reactRouter()],
+  plugins: [
+    devtoolsJson(),
+    tailwindcss(),
+    reactRouterDevTools(),
+    reactRouter(),
+  ],
   server: {
     port: 3000,
   },
